02446e574a36c5b239935f46e075861c33771550
Andres <andressocorro1@gmail.com>
2020-11-21 17:08:26 -0600

    progress

--- D3_Journalism/assets/js/app.js
+++ D3_Journalism/assets/js/app.js
@@ -13,6 +13,37 @@ var margin = {
 var chartWidth = svgWidth - margin.left - margin.right;
 var chartHeight = svgHeight - margin.top - margin.bottom;
 
+// Initial Parameters
+var chosenXAxis = "age";
+var chosenYaxis = "obesity";
+
+// function to update x-scale variable when clicking on axis label
+function xScale(censusData, chosenXAxis){
+
+    // Create scales
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(censusData, d => d[chosenXAxis]) * .08,
+            d3.max(censusData, d[chosenYaxis]) * 1.2
+    ])
+    .range([0, chartWidth]);
+
+    return xLinearScale
+
+}
+
+// function to update y-scale variable when clicking on axis label
+function yScale(censusData, chosenYAxis){
+
+    // create scales
+    var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(censusData, d => d[chosenYAxis]) *1.1
+])
+    .range([chartHeight,0])
+    return yLinearScale;
+}
+
+
+
 // Create SVG wrapperm append a SVG Group that will hold our chart. Shift it by the left and top margins
 
 var svg = d3.select('#scatter')
@@ -22,3 +53,11 @@ var svg = d3.select('#scatter')
 
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+d3.csv("assets/data/data.csv").then(function(censusData){
+
+    console.log(censusData)
+
+
+
+})
